2cad91bca45f3eb431a58dc3a83d71a5dfed2503
Sujan804 <sujanmec804@gmail.com>
2023-04-04 13:20:54 +0600

    React Setup

---
+++ msa/src/App.js
@@ -0,0 +1,38 @@
+import { Route, Routes } from "react-router-dom";
+import AdminLogin from "./pages/admin/AdminLogin";
+import Assignment from "./pages/admin/Assignment";
+import AssignmentMark from "./pages/admin/AssignmentMark";
+import Dashboard from "./pages/admin/Dashboard";
+import Quizzes from "./pages/admin/Quizzes";
+import Videos from "./pages/admin/Videos";
+import CoursePlayer from "./pages/student/CoursePlayer";
+import LeaderBoard from "./pages/student/LeaderBoard";
+import Quiz from "./pages/student/Quiz";
+import StudentLogin from "./pages/student/StudentLogin";
+import StudentReg from "./pages/student/StudentRegistration";
+function App() {
+  return (
+    <div>
+      <Routes>
+        {/* Student Routes */}
+        <Route path="/login" element={<StudentLogin />}></Route>
+        <Route path="/registration" element={<StudentReg />}></Route>
+        <Route path="/leaderbord" element={<LeaderBoard />} />
+        <Route path="/video/:id" element={<CoursePlayer />} />
+        <Route path="/quiz/:id" element={<Quiz />} />
+
+        {/* Admin Routes */}
+        <Route path="/admin">
+          <Route path="" element={<Dashboard />} />
+          <Route path="login" element={<AdminLogin />} />
+          <Route path="assignments" element={<Assignment />} />
+          <Route path="assignment-marks" element={<AssignmentMark />} />
+          <Route path="quizzes" element={<Quizzes />} />
+          <Route path="videos" element={<Videos />} />
+        </Route>
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
---
+++ msa/src/pages/student/StudentRegistration.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import learningportal from "../../image/learningportal.svg";
+const StudentReg = () => {
+  return (
+    <section className="py-6 bg-primary h-screen grid place-items-center">
+      <div className="mx-auto max-w-md px-5 lg:px-0">
+        <div>
+          <img className="h-12 mx-auto" src={learningportal} alt="Msa" />
+          <h2 className="mt-6 text-center text-3xl font-extrabold text-slate-100">
+            Create Your New Account
+          </h2>
+        </div>
+        <form className="mt-8 space-y-6" action="#" method="POST">
+          <input type="hidden" name="remember" value="true" />
+          <div className="rounded-md shadow-sm -space-y-px">
+            <div>
+              <label className="sr-only">Name</label>
+              <input
+                id="name"
+                name="name"
+                type="name"
+                required
+                className="login-input rounded-t-md"
+                placeholder="Student Name"
+              />
+            </div>
+            <div>
+              <label className="sr-only">Email address</label>
+              <input
+                id="email-address"
+                name="email"
+                type="email"
+                required
+                className="login-input "
+                placeholder="Email address"
+              />
+            </div>
+            <div>
+              <label className="sr-only">Password</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                required
+                className="login-input"
+                placeholder="Password"
+              />
+            </div>
+            <div>
+              <label className="sr-only">Confirm Password</label>
+              <input
+                id="confirm-password"
+                name="confirm-password"
+                type="password"
+                required
+                className="login-input rounded-b-md"
+                placeholder="Confirm Password"
+              />
+            </div>
+          </div>
+
+          <div>
+            <button
+              type="submit"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-violet-600 hover:bg-violet-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-violet-500"
+            >
+              Create Account
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default StudentReg;
